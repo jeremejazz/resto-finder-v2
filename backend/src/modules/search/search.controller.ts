c7969dffa9182dd5a6237ec74fcf85ac9bcfd560
Jereme Causing <jeremejazz15@gmail.com>
2025-10-24 09:38:08 +0800

    implement google search

--- backend/src/modules/search/search.controller.ts
+++ backend/src/modules/search/search.controller.ts
@@ -1,14 +1,15 @@
 import { FastifyReply, FastifyRequest } from "fastify"
+import * as searchService from './search.service';
 
 export interface SearchQuery {
     text?: string;
 }
-
-const searchController = async ( request: FastifyRequest<{ Querystring: SearchQuery }>, reply: FastifyReply): Promise<string> => {
-
-    console.log("search controller called")
-    const text = request.query.text;
-    return `search ${text}`;
+const searchController = async ( request: FastifyRequest<{ Querystring: SearchQuery }>, reply: FastifyReply): Promise<any> => {
+    const text = request.query.text || '';
+    const results = await searchService.searchGoogle(text);
+    return {
+        data: results
+    };
 };
 
 export default searchController;
--- backend/src/modules/search/search.service.ts
+++ backend/src/modules/search/search.service.ts
@@ -1,7 +1,28 @@
 
 
-export const searchGoogle = (textQuery: String) =>{
-    // fetch();
+export const searchGoogle = async (textQuery: String) =>{
+
+    const url = process.env.GOOGLE_PLACES_API_URL || "";
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      'Content-Type': 'application/json',
+      "X-Goog-Api-Key": process.env.GOOGLE_API_KEY || "",
+      "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.priceLevel"
+    },
+    body: JSON.stringify({
+        textQuery
+    })
+  };
+
+ 
+    const response = await fetch(url, options);
+
+    const data = response.json();
+
+
+    return data;
 
 }
 
