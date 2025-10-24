cfa2faf34add797aeb65ecc58fa200f1119f33f8
Jereme Causing <jeremejazz15@gmail.com>
2025-10-24 12:34:40 +0800

    add dropdown selection of type

--- frontend/src/components/search/search-box.tsx
+++ frontend/src/components/search/search-box.tsx
@@ -1,8 +1,18 @@
-import { Heading, Input, InputGroup, Stack } from '@chakra-ui/react';
+import { Flex, Heading, Input, InputGroup, NativeSelect, Stack } from '@chakra-ui/react';
 import { VscSparkleFilled } from 'react-icons/vsc';
 import _ from 'lodash';
+import { useRef } from 'react';
 
 const SearchBox = () => {
+
+  const searchRef = useRef<HTMLInputElement>(null);
+  const typeRef = useRef<HTMLSelectElement>(null);
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  
+  };
+
   const randomPlaceholder = _.sample([
     "Cheap ramen near Cebu City that's open now",
     'Best sushi in Tokyo',
@@ -15,20 +25,37 @@ const SearchBox = () => {
       <Heading as="h1" textAlign="center" size="2xl">
         Find the perfect spot
       </Heading>
-      <InputGroup startElement={<VscSparkleFilled />}>
-        <Input
-          type="text"
-          placeholder={randomPlaceholder}
-          borderRadius="full"
-          boxShadow="md"
-          _focus={{
-            borderColor: 'blue.400',
-            boxShadow: 'outline',
-          }}
-        />
-      </InputGroup>
+      <form onSubmit={handleSearch}>
+        <Flex>
+          
+          <InputGroup startElement={<VscSparkleFilled />}>
+            <Input
+              type="text"
+              placeholder={randomPlaceholder}
+              roundedStart='full'
+              boxShadow="md"
+              _focus={{
+                borderColor: 'blue.400',
+                boxShadow: 'outline',
+              }}
+              ref={searchRef}
+            />
+          </InputGroup>
+
+          <NativeSelect.Root size="md" width="200px">
+            <NativeSelect.Field ref={typeRef} placeholder="Select option">
+              <option value="restaurant" selected>Restaurant</option>
+              <option value="bakery">Bakery</option>
+              <option value="coffee_shop">Coffee Shop</option>
+            </NativeSelect.Field>
+            <NativeSelect.Indicator />
+          </NativeSelect.Root>
+        </Flex>
+      </form>
     </Stack>
   );
 };
+
+
 
 export default SearchBox;
