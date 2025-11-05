22533d525a65ce4913d33fe29ef01ff45ceca192
Jereme Causing <jeremejazz15@gmail.com>
2025-11-05 08:28:26 +0800

    update search filter; add search button

--- frontend/src/components/search/search-box.tsx
+++ frontend/src/components/search/search-box.tsx
@@ -1,72 +1,125 @@
-import { Flex, Heading, Input, InputGroup, NativeSelect, Stack } from '@chakra-ui/react';
-import { VscSparkleFilled } from 'react-icons/vsc';
-import _ from 'lodash';
-import { useRef } from 'react';
-
+import {
+  Button,
+  createListCollection,
+  Flex,
+  Heading,
+  HStack,
+  Input,
+  InputGroup,
+  Portal,
+  Select,
+  Stack,
+} from "@chakra-ui/react";
+import { VscSparkleFilled } from "react-icons/vsc";
+import _ from "lodash";
+import { useRef, useState } from "react";
+import SelectTrigger from "../ui/select-trigger";
 
 export interface SearchBoxProps {
-  onSearch?: (query: string,type: string) => void;
+  onSearch?: (query: string, type: string) => void;
 }
 
-const SearchBox = ({ onSearch }: SearchBoxProps) => {
+const searchTypes = createListCollection({
+  items: [
+    { label: "Restaurant", value: "restaurant" },
+    { label: "Bakery", value: "bakery" },
+    { label: "Coffee Shop", value: "coffee_shop" },
+    { label: "Ice Cream Shop", value: "ice_cream_shop" },
+    { label: "Juice Shop", value: "juice_shop" },
+    { label: "Tea House", value: "tea_house" },
+  ],
+});
 
+const SearchBox = ({ onSearch }: SearchBoxProps) => {
   const searchRef = useRef<HTMLInputElement>(null);
-  const typeRef = useRef<HTMLSelectElement>(null);
+  const [searchFilter, setSearchFilter] = useState('restaurant');
+ 
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
-    const query = searchRef.current?.value || '';
-    const type = typeRef.current?.value || 'restaurant';
+    console.log(searchFilter);
+    const query = searchRef.current?.value || "";
+ 
+
+    if (!query || query.length < 2) {
+      return;
+    }
 
     if (onSearch) {
-      onSearch(query, type);
+      onSearch(query, searchFilter);
     }
   };
 
   const randomPlaceholder = _.sample([
     "Cheap ramen near Cebu City",
-    'Best sushi in Tokyo',
-    'Pizza places open late in New York',
-    'Affordable mediterranean restaurant near Ayala Makati'
+    "Best sushi in Tokyo",
+    "Pizza places open late in New York",
+    "Affordable mediterranean restaurant near Ayala Makati",
+    "Best Mexican restaurants Singapore",
+    "Best takeout food near Washington"
   ]);
 
   return (
-    <Stack gap={6} width={{ base: '90%', md: '600px' }}>
+    <Stack gap={6} width={{ base: "90%", md: "600px" }}>
       <Heading as="h1" textAlign="center" size="2xl">
         Find the perfect spot
       </Heading>
       <form onSubmit={handleSearch}>
-        <Flex>
-          
-          <InputGroup startElement={<VscSparkleFilled color='silver' />}>
+        <Flex gap={2} alignItems="center">
+          <InputGroup 
+            startElement={<VscSparkleFilled color="silver" />}
+            flex="1"
+          >
             <Input
               type="text"
               placeholder={randomPlaceholder}
-              roundedStart='full'
+              roundedStart="full"
               boxShadow="md"
               _focus={{
-                borderColor: 'blue.400',
-                boxShadow: 'outline',
+                borderColor: "blue.400",
+                boxShadow: "outline",
               }}
               ref={searchRef}
             />
           </InputGroup>
 
-          <NativeSelect.Root size="md" width="200px"    >
-            <NativeSelect.Field ref={typeRef} defaultValue="restaurant">
-              <option value="restaurant">Restaurant</option>
-              <option value="bakery">Bakery</option>
-              <option value="coffee_shop">Coffee Shop</option>
-            </NativeSelect.Field>
-            <NativeSelect.Indicator />
-          </NativeSelect.Root>
+          <Select.Root
+            positioning={{ sameWidth: false }}
+            collection={searchTypes}
+            defaultValue={["restaurant"]}
+            width="auto"
+            onSelect={({value}) => { setSearchFilter(value)}}
+          >
+            <Select.HiddenSelect />
+            <Select.Control width="auto">
+              <SelectTrigger />
+            </Select.Control>
+            <Portal>
+              <Select.Positioner>
+                <Select.Content minW="32">
+                  {searchTypes.items.map((searchType) => (
+                    <Select.Item item={searchType} key={searchType.value}>
+                      <HStack>{searchType.label}</HStack>
+                      <Select.ItemIndicator />
+                    </Select.Item>
+                  ))}
+                </Select.Content>
+              </Select.Positioner>
+            </Portal>
+          </Select.Root>
+
+          <Button 
+            type="submit" 
+            colorScheme="blue" 
+            variant="surface"
+            flexShrink={0}
+          >
+            Search
+          </Button>
         </Flex>
       </form>
     </Stack>
   );
 };
-
-
 
 export default SearchBox;
---
+++ frontend/src/components/ui/select-trigger.tsx
@@ -0,0 +1,26 @@
+import { IconButton, useSelectContext } from "@chakra-ui/react"
+import { FaFilter } from "react-icons/fa"
+
+const SelectTrigger = () => {
+  const select = useSelectContext()
+  const items = select.selectedItems as Framework[]
+  return (
+    <IconButton
+      px="2"
+      variant="outline"
+      size="sm"
+      {...select.getTriggerProps()}
+    >
+       <FaFilter /> 
+    </IconButton>
+  )
+}
+
+
+interface Framework {
+  label: string
+  value: string
+  icon: React.ReactNode
+}
+
+export default SelectTrigger;
