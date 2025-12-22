9800d8d78630bff6581d3eba32834ea8db9152c8
Jereme Causing <jeremejazz15@gmail.com>
2025-12-22 19:41:34 +0800

    add hide banner configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,30 @@
-import { Box } from '@chakra-ui/react'
-import './App.css'
-import Header from './components/header'
-import Search from './components/search/search'
+import { Box } from "@chakra-ui/react";
+import "./App.css";
+import Header from "./components/header";
+import Search from "./components/search/search";
 
 function App() {
-  
+  const hideBanner = import.meta.env.VITE_HIDE_BANNER;
 
   return (
     <>
+      {hideBanner ? null : (
+        <a
+          className="github-fork-ribbon left-top"
+          href="https://github.com/jeremejazz/resto-finder-v2"
+          target="_blank"
+          data-ribbon="Fork me on GitHub"
+          title="Fork me on GitHub"
+        >
+          Fork me on GitHub
+        </a>
+      )}
       <Box>
         <Header />
         <Search />
       </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
